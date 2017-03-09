a4553a411369a4e46296617f99490ab6682b8e7e
Manish Bodhankar <manish@viromedia.com>
2017-03-09 12:56:48 -0600

    Initial commit for iOS Viro Media App on Beta

---
+++ ios/ViroMediaApp/AppDelegate.m
@@ -0,0 +1,59 @@
+//
+//  AppDelegate.h
+//  ViroMediaApp
+//
+//  Copyright © 2017 Viro Media. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import <ViroReact/VRTBundleURLProvider.h>
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+
+
+
+@implementation AppDelegate
+
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  
+//  BOOL enterVrImmediately = YES;
+//  BOOL usingNgrok = YES;
+//  
+//  if(enterVrImmediately) {
+//    NSURL *jsCodeLocation = nil;
+//    
+//#ifdef DEBUG
+//    if(usingNgrok) {
+//      VRTBundleURLProvider *bundleProvider = [[VRTBundleURLProvider alloc] init];
+//      jsCodeLocation = [bundleProvider jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+//    }
+//#endif
+//    
+//    if(jsCodeLocation == nil) {
+//      jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+//    }
+//    
+//    RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+//                                                        moduleName:@"ViroMediaAppBeta"
+//                                                 initialProperties:nil
+//                                                     launchOptions:launchOptions];
+//    rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+//    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+//    UIViewController *rootViewController = [UIViewController new];
+//    rootViewController.view = rootView;
+//    self.window.rootViewController = rootViewController;
+//    [self.window makeKeyAndVisible];
+//  }
+  return YES;
+}
+
+// This function determines what orientation our app supports whereas the orientation in the plist determines which orientation we are allowed
+// to launch into. If this function isn't specified then the plist is the default. Also UIViewControllers can override this too.
+- (UIInterfaceOrientationMask)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window {
+  return UIInterfaceOrientationMaskAllButUpsideDown;
+}
+
+@end
