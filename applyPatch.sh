64388dd7ef605b26f6dbdbc05c650f3053638244
Manish Bodhankar <manish@viromedia.com>
2017-11-28 16:52:50 -0800

    Checking in fastlane scripts for android test bed

--- applyPatch.sh
+++ applyPatch.sh
@@ -2,4 +2,4 @@
 
 patch -i changes/3940c3940.diff node_modules/react-native/React/Base/RCTDefines.h
 patch -i changes/129c129.diff node_modules/react-native/React/Base/RCTAssert.m
-
+patch -i changes/32f1851.diff node_modules/react-native/ReactAndroid/release.gradle
