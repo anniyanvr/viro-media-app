f2f4761076f361aed2afe1bf54b9b9dcc9cf7f13
Manish Bodhankar <manish@viromedia.com>
2018-01-15 18:26:59 -0800

    Updating pod files

--- ios/Pods/Headers/Public/ViroReact/ViroKit/VROAction.h
+++
@@ -1,1 +0,0 @@
-../../../../../../node_modules/react-viro/ios/dist/ViroKit.framework/Headers/VROAction.h
--- ios/ViroMediaApp/TestbedEntryViewController.m
+++ ios/ViroMediaApp/TestbedEntryViewController.m
@@ -12,7 +12,7 @@
 
 static NSString *const kInvalidEndpointMessage = @"[%@] is an invalid endpoint! Please enter an IP Address between 0.0.0.0 and 255.255.255.255 or a ngrok endpoint of the form 'xxxxx.ngrok.io'";
 
-static NSString *const kVersionText = @"React-Viro v2.2.0";
+static NSString *const kVersionText = @"React-Viro v2.3.0";
 static NSString *const kReleaseNotes = @"Release Notes";
 
 // a validendpoint starts with `https://`.
