37473371b4e26e83d3bb16e588601584c7defe53
Vik Advani <vadvani@viromedia.com>
2018-03-20 20:26:49 -0700

    VIRO-3056 - Fixed various testbed issues.
    Mainly fixed cache issues so testbed no reloads from package server everytime.
    Also fixed system bottom nav menu, so it now shows on the main screen.

--- applyPatch.sh
+++ applyPatch.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+patch -i changes/411c411c.diff node_modules/react-native/ReactAndroid/src/main/java/com/facebook/react/ReactInstanceManager.java
 patch -i changes/3940c3940.diff node_modules/react-native/React/Base/RCTDefines.h
 patch -i changes/129c129.diff node_modules/react-native/React/Base/RCTAssert.m
 patch -i changes/32f1851.diff node_modules/react-native/ReactAndroid/release.gradle
